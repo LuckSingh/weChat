70fd0e33f10447931e12cbfe13fac3268a31b0b9
enterpress <xinge.tong@bluegogo.com>
2017-09-14 10:16:18 +0800

    修改less

--- src/components/Index/IndexHeader.vue
+++ src/components/Index/IndexHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="indexHeader">
-  {{msg}}
+  	<a class="unbundling" href="unbundling.html">解绑账号</a>
+  	<a class="time" href="javascript:;" id="open">{{time}}</a>
   </div>
 </template>
 <script>
@@ -8,13 +9,42 @@ export default {
   name: 'indexHeader',
   data () {
     return {
-      msg: '首页-头部'
+      time: '2017-27-14'
     }
   }
 }
 </script>
 <style scoped lang='less'>
 .indexHeader{
-  color:red;
+  	width: 100%;
+    height: 7.9227rem;
+    background: #499a76;
+    position: relative;
+    background: url(../../assets/524980347691454594.png)no-repeat 0 0;
+    background-size: cover;
+    .unbundling{
+    	position: absolute;
+	    left: 0.438rem;
+	    top: 0.438rem;
+	    background: url(../../assets/icon_home_lock.png) no-repeat 0 0;
+	    background-size: 0.8244rem 0.8244rem;
+	    height: 0.8244rem;
+	    line-height: 0.8244rem;
+	    font-size: 0.6183rem;
+	    padding-left: 1.0048rem;
+	    color: #fff;
+    }
+    .time{
+    	position: absolute;
+	    right: 0.438rem;
+	    top: 0.438rem;
+	    background: url(../../assets/icon_home_date.png) no-repeat 0 0;
+	    background-size: 0.8244rem 0.8244rem;
+	    height: 0.8244rem;
+	    line-height: 0.8244rem;
+	    font-size: 0.6183rem;
+	    padding-left: 1.0048rem;
+	    color: #fff;
+    }
 }
 </style>
